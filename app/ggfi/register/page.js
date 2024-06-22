0b3bf12f86d73f81029f783afe881aaa04b57542
Asep Irman <asepirman@antigravity.id>
2024-06-22 08:59:57 +0700

    init

---
+++ app/ggfi/register/page.js
@@ -0,0 +1,86 @@
+'use client';
+
+import Image from "next/image";
+import { useEffect, useState } from 'react';
+import { setCookie } from 'cookies-next';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import { Poppins} from "next/font/google";
+const poppins = Poppins({ subsets: ["latin"], weight: ['400','700', '900'] });
+
+export default function Register() {
+    const router = useRouter();
+    const [payload, setPayload] = useState({
+      name: '',
+      phone: '',
+    });
+
+
+    useEffect(() => {
+        document.getElementById('phone').focus(); 
+    }, [])
+
+
+    const isValid = () => {
+      if (payload.phone) return true
+      else return false;
+    };
+
+    const handleChange = (e) => {
+        const { value, name } = e.target;
+        setPayload((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+    };
+
+    const handleSubmit = () => {
+        setCookie('phone', payload.phone);
+        router.push('/cam');
+        // setTimeout(() => {
+        //     router.push('/cam');
+        // }, 250);
+    }
+    return (
+        <main className="flex fixed h-full w-full bg-page overflow-auto flex-col items-center justify-center pt-2 pb-2 px-5 lg:pt-0 lg:px-20 mt-0">
+            <div className='fixed w-full mx-auto flex justify-center items-center pointer-events-none top-0 left-0 right-0'>
+                <Image src='/top-logo.png' width={1179} height={246} alt='Zirolu' className='w-full' priority />
+            </div>
+            <div className="relative w-full flex flex-col justify-center items-center mt-16">
+                <div className='relative w-[90%] mb-6'>
+                    <label htmlFor="name" className={`text-light font-bold text-xl mb-4 block ${poppins.className}`}>Phone Number</label>
+                    <div className='relative w-full'>
+                        <Image
+                            src='/icon-call.png'
+                            width={32}
+                            height={32}
+                            className='absolute left-3 top-1/2 -translate-y-1/2 w-[18px]'
+                            alt='icon'
+                        />
+                        <p className={`absolute left-[2.1rem] top-1/2 font-bold text-base text-black -translate-y-1/2 ${poppins.className}`}>+62</p>
+                        <input
+                            type='number'
+                            value={payload.phone}
+                            id='phone'
+                            name='phone'
+                            className={`w-full rounded-lg font-semibold text-base outline-none py-3 pr-3 pl-[4.5rem] text-black bg-light ${poppins.className}`}
+                            placeholder='Your number'
+                            onChange={handleChange}
+                        />
+                    </div>
+                    {/* {payload.phone} */}
+                    {/* {errorMsg && <p className='text-[#E00A0A] text-xs'>{errorMsg}</p>} */}
+                </div>
+            </div>
+            <div className="relative w-[90%] mx-auto flex justify-center items-center flex-col">
+                <button className={`w-full relative mx-auto flex justify-center items-center ${payload.phone ? '' : 'pointer-events-none opacity-10'}`} onClick={handleSubmit}>
+                    <Image src='/btn-next.png' width={867} height={163} alt='Zirolu' className='w-full' priority />
+                </button>
+                <Link href='/' className="relative w-full mx-auto flex justify-center items-center pt-6">
+                    <Image src='/btn-back.png' width={867} height={163} alt='Zirolu' className='w-full' priority />
+                </Link>
+            </div>
+        </main>
+    );
+}
---
+++ app/ggfi/result/page.js
@@ -0,0 +1,218 @@
+'use client';
+
+import Link from 'next/link';
+import Image from "next/image";
+import { getCookie } from 'cookies-next';
+import React,{ useEffect, useState, useRef } from 'react';
+import { useQRCode } from 'next-qrcode';
+// import io from 'socket.io-client';
+import { Poppins} from "next/font/google";
+const poppins = Poppins({ subsets: ["latin"], weight: ['400','700', '900'] });
+// import BtnHexagon2 from "../components/BtnHexagon2";
+// import ReactToPrint from "react-to-print";
+
+
+export default function Result() {
+    const [imageResultAI, setImageResultAI] = useState(null);
+    const [generateQR, setGenerateQR] = useState(null);
+    const [linkQR, setLinkQR] = useState('https://zirolu.id/');
+    const [loadingDownload, setLoadingDownload] = useState(null);
+    const [styleGender, setStyleGender] = useState(null);
+    let componentRef = useRef();
+
+    const [payload, setPayload] = useState({
+        name: 'GGIF',
+        phone: getCookie('phone'),
+    });
+    const { Canvas } = useQRCode();
+
+    useEffect(() => {
+        // Perform localStorage action
+        if (typeof localStorage !== 'undefined') {
+            const item = localStorage.getItem('resulAIBase64')
+            const item2 = localStorage.getItem('styleGender')
+            const item3 = localStorage.getItem('faceURLResult')
+            setImageResultAI(item)
+            setStyleGender(item2)
+            setLinkQR(item3)
+        }
+    }, [imageResultAI, styleGender, linkQR])
+
+    const downloadImageAI = () => {
+        // gtag('event', 'ClickButton', {
+        //     event_category: 'Button',
+        //     event_label: 'ResultPage - '+payload.stasiunName,
+        //     event_action: 'CollectYourPhoto'
+        // })
+        
+        // import('html2canvas').then(html2canvas => {
+        //     html2canvas.default(document.querySelector("#capture"), {scale:4}).then(canvas => 
+        //         uploadImage(canvas)
+        //     )
+        // }).catch(e => {console("load failed")})
+        setGenerateQR('true')
+    }
+    const uploadImage = async (canvas) => {
+        setLoadingDownload('≈')
+
+        canvas.toBlob(async function(blob) {
+            let bodyFormData = new FormData();
+            bodyFormData.append("name", payload.name+' - '+styleGender);
+            bodyFormData.append("phone", payload.phone);
+            bodyFormData.append("type", 'ggif');
+            bodyFormData.append("fromPhone", true);
+            bodyFormData.append("file", blob, payload.name+'-ai-zirolu.png');
+          
+            const options = {
+                method: 'POST',
+                body: bodyFormData,
+                headers: {
+                    'Authorization': '89d183b7-ce47-4ceb-8676-1c2378f5be19:wZgrzLLKXOIjgACeJWH34iwOGqVZQmVg',
+                    'Accept': 'application/json',
+                }
+            };
+            
+            await fetch('https://api.priapunyaselera.ai/v1/photoai', options)
+                .then(response => response.json())
+                .then(response => {
+                    // console.log(response)
+                    // console.log(response.file)
+                    setLinkQR(response.file)
+                    setGenerateQR('true')
+                    setLoadingDownload(null)
+                })
+                .catch(err => {
+                    if (typeof localStorage !== 'undefined') {
+                        const item = localStorage.getItem('faceURLResult')
+                        setLinkQR(item)
+                        setGenerateQR('true')
+                        setLoadingDownload(null)
+                    }
+                });
+        });
+    }
+    const backHome = () => {
+        gtag('event', 'ClickButton', {
+            event_category: 'Button',
+            event_label: 'ResultPage - '+payload.stasiunName,
+            event_action: 'BackToHome'
+        })
+    }
+
+    
+
+    return (
+        <main className="flex fixed h-full w-full bg-page overflow-auto flex-col justify-center items-center py-5 px-5 lg:py-16 lg:px-20" onContextMenu={(e)=> e.preventDefault()}>
+            {/* QR */}
+            {generateQR && 
+                <div className='absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center flex-col z-40 bg-kai3 text-black bg-opacity-0'>
+                    <div className='fixed w-full mx-auto flex justify-center items-center pointer-events-none top-0 left-0 right-0'>
+                    <Image src='/top-logo.png' width={1179} height={246} alt='Zirolu' className='w-full' priority />
+                    </div>
+                    {/* <div className='fixed top-0 mx-auto w-[65%] mt-28'>
+                        <Image src='/title-scan.png' width={815} height={195} alt='Zirolu' className='w-full' priority />
+                    </div>
+                    <div className='relative mt-[-14vh] w-[60%] mx-auto flex items-center justify-center canvas-qr border-4 border-black' onClick={()=>{setGenerateQR(null)}}>
+                        <Canvas
+                        text={linkQR}
+                        options={{
+                            errorCorrectionLevel: 'M',
+                            margin: 3,
+                            scale: 4,
+                            width: 500,
+                            color: {
+                            dark: '#000000',
+                            light: '#ffffff',
+                            },
+                        }}
+                        />
+                    </div> */}
+                    <p className={`text-center font-semibold text-base px-5 text-white ${poppins.className}`}>Download the image below</p>
+                    <div className={`relative w-full mb-8`}>
+                    <div className="relative w-[70%] mx-auto flex justify-center items-center flex-col mt-2">
+                        <a href={linkQR} target='_blank' className="relative mx-auto flex justify-center items-center">
+                            <Image src='/btn-download2.png' width={632} height={105} alt='Zirolu' className='w-full' priority />
+                        </a>
+                    </div>
+                    </div>
+                    <p className={`text-center font-semibold text-base px-5 text-white ${poppins.className}`}>Or you can print at <br></br>the Automatic Print Station</p>
+                    {/* <Link href='/' className='text-center font-semibold text-lg mt-2 p-20' onClick={()=>{setGenerateQR(null)}}>Tap here to close</Link> */}
+                    {/* <a href='/home' className='text-center font-semibold text-4xl py-20 pb-36 p-40'>Tap here to close</a> */}
+
+                    <div className={`fixed left-0 bottom-10 w-full`}>
+                        <div className="relative w-[80%] mx-auto flex justify-center items-center flex-col" onClick={backHome}>
+                            <Link href='/' className="relative w-full mx-auto flex justify-center items-center pb-14">
+                                <Image src='/btn-back.png' width={772} height={135} alt='Zirolu' className='w-full' priority />
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+            }
+            {/* QR */}
+
+
+            {/* DOWNLOAD & PRINT */}
+            {/* {imageResultAI && 
+            <div className='relative w-full mt-0 mb-0 mx-auto flex justify-center items-center opacity-0 pointer-events-none'>
+                <div className='absolute z-10 w-[20%]' id='capture'>
+                    <div className={`relative w-[full] flex`}>
+                        <Image src={imageResultAI}  width={630} height={1120} alt='Zirolu' className='relative block w-full'></Image>
+                    </div>
+                </div>
+                <div className='absolute top-0 left-0  w-full' ref={(el) => (componentRef = el)}>
+                    <div className={`relative w-[99%] flex`}>
+                        <Image src={imageResultAI}  width={683} height={1024} alt='Zirolu' className='relative block w-full'></Image>
+                    </div>
+                </div>
+            </div>
+            } */}
+
+            <div className={generateQR ? `opacity-0 pointer-events-none w-full` : 'w-full mt-[-3rem]'}>
+                {imageResultAI && 
+                <div className='relative w-[98%] mx-auto flex justify-center items-center rounded-sm' id='capture' onClick={downloadImageAI}>
+                    <div className='relative w-full'>
+                        <Image src={imageResultAI}  width={1024} height={683} alt='Zirolu' className='relative block w-full'></Image>
+                    </div>
+                </div>
+                }
+                {loadingDownload && 
+                    <div className='rrelative p-3 mt-5 border-2 border-[#b1454a] text-center bg-[#CF1F29] text-[#fff] text-base overflow-auto no-scrollbar w-[70%] mx-auto rounded-lg'>
+                        <p>Please wait, loading...</p>
+                    </div>
+                }
+                <div className={`relative w-full ${loadingDownload ? 'hidden' : ''}`}>
+                    <div className={`w-full`}>
+                        <div className={`w-full mt-6`}>
+                            <div className="relative w-[90%] mx-auto flex justify-center items-center">
+                                <button className="w-full relative mx-auto flex justify-center items-center" onClick={downloadImageAI}>
+                                    <Image src='/btn-download.png' width={515} height={164} alt='Zirolu' className='w-full' priority />
+                                </button>
+
+
+                                {/* <div className={`w-full`} onClick={downloadImageAI}>
+                                <ReactToPrint
+                                trigger={() => 
+                                    <div className={`w-full`}>
+                                        <div className="w-full relative mx-auto flex justify-center items-center">
+                                            <Image src='/btn-download2.png' width={1265} height={211} alt='Zirolu' className='w-full' priority />
+                                        </div>
+                                    </div>
+                                }
+                                content={() => componentRef}
+                                />
+                                </div>  */}
+
+                                <Link href='/generate' className="relative w-full mx-auto flex justify-center items-center pl-4">
+                                    <Image src='/btn-retake.png' width={515} height={164} alt='Zirolu' className='w-full' priority />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="fixed w-full bottom-0 left-0 mx-auto pointer-events-none">
+                <Image src='/print-your.png' width={589} height={102} alt='Zirolu' className='w-full' priority />
+            </div>
+        </main>
+    );
+}
